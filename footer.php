8876ca9be08d5c1ef7b93f869880066463f64421
Dafidillo <rodgers.marco@hotmail.com>
2021-05-23 12:22:37 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,21 @@
+<Footer class="container-fluid">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-6 text-center footer-content">
+                  <p> <?php bloginfo('name'); ?></p>
+                    <img width="40" alt="b" src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/">
+                     <img width="40" alt="b" src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/">
+
+                </div>
+                <div class="col-md-6 text-center footer-content">
+                    <p>Terms and Conditions</p>
+                     <img width="40" alt="b" src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/">
+                     <img width="40" alt="b" src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/">
+                </div>
+            </div>
+        </div>
+    </Footer>
+</body>
+</html>
+<?php wp_footer(); ?>
