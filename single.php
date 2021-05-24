e60839faa18f6b1063a0273ee21af5f0b2c2a837
Dafidillo <rodgers.marco@hotmail.com>
2021-05-24 13:40:06 +0800

    Update single.php

--- single.php
+++ single.php
@@ -1,47 +1,67 @@
-<?php get_header(); /* Tells WordPress to include header.php */ ?>
-<section class="container-fluid aboutbg text-center">
-        <div class="container">
-            <h2> GAMES &amp; LEVEL DESIGNER</h2>
-            <p class="particle">...and particle wizard</p>
-            <p class="about text">My name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments.
-                I always tell a story.</p>
+<<?php // Do not delete these lines
+if ('comments.php' == basename($_SERVER['SCRIPT_FILENAME'])) die ('Please do not load this page directly. Thanks!');
+/* Variable for alternating comment background */
+$oddcomment = 'alt';
+?>
+<!-- You can start editing here. -->
+<?php if ($comments) : ?>
+<h3 id="comments"><?php comments_number('No Comments', '1 Comment', '% Comments');?></h3>
+<ol class="commentlist">
+<?php foreach ($comments as $comment) : ?>
+<li class="<?php /* Separate admin comments */ if ($comment->user_id) echo 'authcomment'; else echo $oddcomment; ?>" id="comment-<?php comment_ID() ?>">
+<div class="commentmetadata">
+<span class="comment-author"><?php comment_author_link() ?></span>
+<span class="comment-date"><?php comment_date('F jS, Y') ?> at <?php comment_time() ?></span>
+                
+<?php if ($comment->comment_approved == '0') : ?>
+<span class="comment-mod"><em>Your comment is awaiting moderation.</em></span>
+<?php endif; ?>
+</div>
+            
+<div class="comment-text">
+<?php comment_text() ?>
+</div>
+<div style="clear:both;"></div>
+</li>
+<?php /* Changes every other comment to a different class */
+if ('alt' == $oddcomment) $oddcomment = '';
+else $oddcomment = 'alt';
+?>
+<?php endforeach; /* end for each comment */ ?>
+</ol>
 
-        </div>
-        <div class="blocker"></div>
-        <!--1st container-->
-    </section>
-    <section class="container-fluid articlebg">
-        <div class="container">
-            <div class="row"></div>
-            <div class="article-title text-center">
-                <h3>Lastest Articles </h3>
-            </div>
-            <!--1ST CONTAINER-->
-            <div class="col-md-4 article-cent">
-                <img class="img-responive" alt="portrait" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/ game.jpg">
-                <h4>A PORTRAIT</h4>
-                <p class="date">DECEMBER,NOVEMEBER 10 2019</p>
-                <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
-                <p class="readmore">CONTINUE READING</p>
-            </div>
-            <!--SECOND CONTAINER-->
-            <div class="col-md-4 article-cent">
-                <img class="img-responive" alt="portrait" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/ game.jpg">
-                <h4>A PORTRAIT</h4>
-                <p class="date">DECEMBER,NOVEMEBER 10 2019</p>
-                <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
-                <p class="readmore">CONTINUE READING</p>
-            </div>
-            <!--THRID CONTAINER-->
-            <div class="col-md-4 article-cent">
-                <img class="img-responive" alt="portrait" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/ game.jpg">
-                <h4>A PORTRAIT</h4>
-                <p class="date">DECEMBER,NOVEMEBER 10 2019</p>
-                <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
-                <p class="readmore">CONTINUE READING</p>
-            </div>
+<?php else : /* this is displayed if there are no comments so far */ ?>
+    
+<h3 id="comments">No comments</h3>
+<?php if ('open' == $post->comment_status) : ?>
+<!-- If comments are open, but there are no comments. -->
+<p class="nocomments">No comments yet.</p>
+<?php else : // comments are closed ?>
+<!-- If comments are closed. -->
+<p class="nocomments">Comments are closed.</p>
+<?php endif; ?>
 
-        </div>
-
-    </section>
-<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
+<?php endif; ?>
+<?php if ('open' == $post->comment_status) : ?>
+<h3 id="respond">Leave a Comment</h3>
+<?php if ( get_option('comment_registration') && !$user_ID ) : ?>
+<p>You must be <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=<?php the_permalink(); ?>">logged in</a> to post a comment.</p>
+<?php else : ?>
+<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+<p><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="40" tabindex="1" />
+<label for="author"><small>Name <?php if ($req) echo "(required)"; ?></small></label></p>
+<p><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="40" tabindex="2" />
+<label for="email"><small>Mail (will not be published) <?php if ($req) echo "(required)"; ?></small></label></p>
+<p><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="40" tabindex="3" />
+<label for="url"><small>Website</small></label></p>
+<p><textarea name="comment" id="comment" cols="60" rows="10" tabindex="4"></textarea></p>
+        
+<p class="allowed-tags"><small><strong>XHTML: </strong>You can use these tags&#58;<code><?php echo allowed_tags(); ?></code></small></p>
+ 
+<p><input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
+<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
+</p>
+<?php do_action('comment_form', $post->ID); ?>
+</form>
+<?php endif; // If registration required and not logged in ?>
+<?php endif; // if you delete this the sky will fall on your head ?>
